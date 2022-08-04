6160259a3eccd43a6bd79fd3871645d69a3a77f0
Victor Nunes <urubuis1@gmail.com>
2022-08-04 12:27:28 -0400

    All tests passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+	let sum = 0;
+	let menor = 0;
+	let maior = 0;
 
+	if (
+		firstNumber < 0 ||
+		secondNumber < 0 ||
+		typeof secondNumber != "number" ||
+		typeof firstNumber != "number"
+	)
+		return "ERROR";
+	if (secondNumber > firstNumber) {
+		menor = firstNumber;
+		maior = secondNumber;
+	} else {
+		menor = secondNumber;
+		maior = firstNumber;
+	}
+	for (; menor <= maior; menor++) {
+		sum += menor;
+	}
+	return sum;
 };
 
 // Do not edit below this line
