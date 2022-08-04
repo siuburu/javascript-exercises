a36bb88386d57bc43451633e28b197f89901add5
Victor <urubuis1@gmail.com>
2022-08-04 18:14:42 -0400

    All tests passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (cTemp) {
+	return Math.round((cTemp - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (fTemp) {
+	return Math.round((fTemp * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
